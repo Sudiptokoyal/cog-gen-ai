803ab9e127a65ae1ed143db34b676cc127d6f8b9
sudipto <sudipto.k@mucrest.com>
2023-11-27 17:55:03 +0530

    added basic core comps

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -3,4 +3,25 @@ import "../styles/global.css";
 export { cn } from "./lib/utils";
 
 export { Button } from "./components/button";
-export { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from "./components/accordion"
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from "./components/accordion";
+export { Avatar, AvatarImage, AvatarFallback } from "./components/avatar";
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "./components/card";
+export { Badge, badgeVariants } from "./components/badge";
+export { Input } from "./components/input";
+export { Label } from "./components/label";
+export { Textarea } from "./components/textarea";
+export { RadioGroup, RadioGroupItem } from "./components/radioGroup"
+
+export {
+    Dialog,
+    DialogPortal,
+    DialogOverlay,
+    DialogClose,
+    DialogTrigger,
+    DialogContent,
+    DialogHeader,
+    DialogFooter,
+    DialogTitle,
+    DialogDescription,
+  } from "./components/dialog";
+  
