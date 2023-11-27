03ea42c4165c9817e75b10b2d819f384d2bc6cdd
sudipto <sudipto.k@mucrest.com>
2023-11-27 15:21:21 +0530

    accordian added

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -3,3 +3,4 @@ import "../styles/global.css";
 export { cn } from "./lib/utils";
 
 export { Button } from "./components/button";
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from "./components/accordion"
